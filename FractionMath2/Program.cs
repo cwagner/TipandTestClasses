966d52f60c84baf3839a5215e0f675d0dd8a2209
Steve Conger <steve@spconger.com>
2018-05-29 14:33:10 -0700

    Add project files.

---
+++ FractionMath2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FractionMath2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction f1 = new Fraction("1/3");
+            Fraction f2 = new Fraction("1/2");
+            Fraction f3 = f1 + f2;
+            Console.WriteLine(
+                f1.ToString() + " + " + f2.ToString() + "=" + f3.ToString());
+            Fraction f4 = f1 * f2;
+            Console.WriteLine(
+                f1.ToString() + " * " + f2.ToString() + "=" + f4.ToString());
+          
+            if (f1 > f2)
+                Console.WriteLine(f1.ToString() + ">" + f2.ToString());
+            else if (f1 < f2)
+                Console.WriteLine(f1.ToString() + "<" + f2.ToString());
+            else
+                Console.WriteLine(f1.ToString() + "=" + f2.ToString());
+            Console.ReadKey();
+        }
+        
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FractionMath2;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAddition()
+        {
+            Fraction f1 = new Fraction(1, 2);
+            Fraction f2 = new Fraction("1/4");
+            Fraction f3 = f1 + f2;
+            Assert.AreEqual(f3.ToString(), "3/4");
+        }
+
+        [TestMethod]
+        public void TestComparitive()
+        {
+            Fraction f1 = new Fraction(1, 2);
+            Fraction f2 = new Fraction("1/4");
+           
+            Assert.IsTrue(f1>f2);
+        }
+
+    }
+
+
+}
